01dd2eb5b4a842e31b7314afef903b68fce8cf79
郑康 <1473345713@qq.com>
2018-09-05 13:09:46 +0800

    Update IPconvert.c

--- IPconvert.c
+++ IPconvert.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdbool.h>
+#include<stdlib.h>
 typedef unsigned int uint32_t;
 typedef unsigned char u_char;
+uint32_t ngx_convert_ip_to_uint(char* base) ;
+bool ngx_is_black_ip_style_two(char* base, char* src);
+bool ngx_is_black_ip_style_one(char* base, char* src);
 int main()
 {
-
+    char s[30], r[30], *pos;
+    bool ans;
+    scanf("%s\n%s", &s, &r);
+    pos = strchr(s, '/');
+    if (pos) {
+         ans = ngx_is_black_ip_style_one(s, r);
+    } else {
+         ans = ngx_is_black_ip_style_two(s, r);
+    }
+    printf("ans = %d\n", ans);
     return 0;
 }
 //192.168.1.1-254
@@ -13,15 +27,15 @@ bool ngx_is_black_ip_style_two(char* base, char* src)
 	int num = 0, len, len_of_base;
 	char* ip;
 	uint32_t res[3] = {0,0,0};
-	u_char	*pos;
-	pos = base;	
-	pos = (u_char*)ngx_strchr(base, '-');
-	ip = base;
+	char	*pos;
+	ip = pos = base;	
+	pos = strchr(base, '-');
+    *pos = '\0';
 	len = pos - base;
-    len_of_base = strlen(base);
+    //len_of_base = strlen(base);
 	//IP 下限
 	res[0] = ngx_convert_ip_to_uint(ip);
-	num = ngx_atoi(pos+1, len_of_base - len -1);
+	num = atoi(pos+1);
 	printf("num = %d\n", num);	
 	//IP上限	
 	res[1] = res[0] + num;
@@ -41,15 +55,15 @@ bool ngx_is_black_ip_style_one(char* base, char* src)
 	int num = 0, len, len_of_base;
 	char* ip;
 	uint32_t res[3] = {0,0,0};
-	u_char	*pos;
+	char	*pos;
 	pos = base;	
-	pos = (u_char*)ngx_strchr(base, '/');
+	pos = (u_char*)strchr(base, '/');
 	ip = base;
 	len = pos - base;
-       len_of_base = strlen(base);
+    len_of_base = strlen(base);
 	//IP 下限
 	res[0] = ngx_convert_ip_to_uint(ip);
-	num = ngx_atoi(pos+1, len_of_base - len -1);
+	num = atoi(pos+1);
 	printf("num = %d\n", num);	
 	//IP上限	
 	res[1] = res[0] + (1<<(32 - num));
@@ -66,22 +80,23 @@ bool ngx_is_black_ip_style_one(char* base, char* src)
 //IP 转换成数字
 uint32_t ngx_convert_ip_to_uint(char* base) 
 {
-	int i = 0, len = 0;
-	u_char	*pos, *index;
+	int      i = 0, len = 0;
+	char	*pos, *index;
 	index = pos = base;
-	uint32_t res=0;	
-	while (i < strlen(base)) {
+	uint32_t res = 0;	
+	while (*pos) {
 		if(*pos == '.') {
-			len = pos - index;
-			res = (res << 8) + ngx_atoi(index, len);
+			//len = pos - index;
+            *pos = '\0';
+			res = (res << 8) + atoi(index);
 			printf("res = %u\n",res);
 			index = pos+1;
 		}
 		pos++;
-		i++;
 	}
 	len = pos - index;
 	if(len)
-		res = (res << 8) + ngx_atoi(index, len);	
+		res = (res << 8) + atoi(index);	
 	return res;
 }
+
