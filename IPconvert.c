fd1cbb6711652a87bdd59eba3adcdd5826b88142
zhengkang <1473345713@qq.com>
2018-09-06 16:16:10 +0800

    IP数字转换

--- IPconvert.c
+++ IPconvert.c
@@ -18,75 +18,67 @@ int main()
     } else {
          ans = ngx_is_black_ip_style_two(s, r);
     }
-    printf("ans = %d\n", ans);
+    printf("\nans = %d\n", ans);
     return 0;
 }
 //192.168.1.1-254
 bool ngx_is_black_ip_style_two(char* base, char* src)
 {
-	int num = 0, len, len_of_base;
-	char* ip;
-	uint32_t res[3] = {0,0,0};
-	char	*pos;
-	ip = pos = base;	
+	int 		 num = 0, len, len_of_base;
+	char	    *pos;
+	uint32_t 	 res[3] = {0,0,0};
+	
 	pos = strchr(base, '-');
     *pos = '\0';
-	len = pos - base;
-    //len_of_base = strlen(base);
 	//IP 下限
-	res[0] = ngx_convert_ip_to_uint(ip);
+	res[0] = ngx_convert_ip_to_uint(base);
 	num = atoi(pos+1);
 	printf("num = %d\n", num);	
 	//IP上限	
 	res[1] = res[0] + num;
 	//待匹配IP
 	res[2] = ngx_convert_ip_to_uint(src);
-	printf("res=%u\t%u\t%u",res[0],res[1],res[2]);
-	//printf("res = %u\n", res);
+	printf("res = %u\t%u\t%u\n", res[0], res[1], res[2]);
 	if (res[2] > res[0] && res[2] < res[1])
 		return true;
 	return false;
-	//return res[0];
+
 }
 
 //判断安全组黑名单Ip 192.168.0.1/24
 bool ngx_is_black_ip_style_one(char* base, char* src)
 {
-	int num = 0, len, len_of_base;
-	char* ip;
-	uint32_t res[3] = {0,0,0};
-	char	*pos;
-	pos = base;	
+	int 		 num = 0, len;
+	char		*pos;
+	uint32_t 	 res[3] = {0,0,0};
+
 	pos = (u_char*)strchr(base, '/');
-	ip = base;
-	len = pos - base;
-    len_of_base = strlen(base);
+	*pos = '\0';
 	//IP 下限
-	res[0] = ngx_convert_ip_to_uint(ip);
+	res[0] = ngx_convert_ip_to_uint(base);
 	num = atoi(pos+1);
 	printf("num = %d\n", num);	
 	//IP上限	
-	res[1] = res[0] + (1<<(32 - num));
+	res[1] = res[0] + (1 << (32 - num));
 	//待匹配IP
 	res[2] = ngx_convert_ip_to_uint(src);
-	printf("res=%u\t%u\t%u",res[0],res[1],res[2]);
-	//printf("res = %u\n", res);
+	printf("res = %u\t%u\t%u\n", res[0], res[1], res[2]);
 	if (res[2] > res[0] && res[2] < res[1])
 		return true;
 	return false;
-	//return res[0];
+
 }
 
 //IP 转换成数字
 uint32_t ngx_convert_ip_to_uint(char* base) 
 {
-	int      i = 0, len = 0;
-	char	*pos, *index;
+	int         i = 0;
+	char	   *pos, *index;
+	uint32_t    res = 0;	
+
 	index = pos = base;
-	uint32_t res = 0;	
 	while (*pos) {
-		if(*pos == '.') {
-			//len = pos - index;
+		if (*pos == '.') {
             *pos = '\0';
 			res = (res << 8) + atoi(index);
 			printf("res = %u\n",res);
@@ -94,9 +86,7 @@ uint32_t ngx_convert_ip_to_uint(char* base)
 		}
 		pos++;
 	}
-	len = pos - index;
-	if(len)
-		res = (res << 8) + atoi(index);	
+	res = (res << 8) + atoi(index);	
 	return res;
 }
 
