bb6cb7b0f3313327979fd09c5203eccf1c5e5611
zhengkang <1473345713@qq.com>
2018-09-06 15:03:56 +0800

    704. Binary Search

---
+++ Python/704.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
+       
+
+
+nums =[1,2,3]
+target = 1
