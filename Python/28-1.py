be0290cb1cbe2a6448a2b2650c9eeb1f2465ec4a
zhengkang <1473345713@qq.com>
2018-09-09 09:14:27 +0800

    28. Implement strStr() 解法2

---
+++ Python/28-1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
+res = Solution()
+ans = res.strStr("mississiplis", "issip")
+print(ans)
+
