a4514188356bfbee92712b251742cc96a3e04cb4
郑康 <zhengkang@idss-cn.com>
2018-12-29 13:36:28 +0800

    6. ZigZag Conversion

---
+++ Python/6.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        L = [''] * numRows
+        index, step = 0, 1
+
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+
+        return ''.join(L)
+
+if __name__ == '__main__':
+    res = Solution()
+    s = "PAYPALISHIRING"
+    l = res.convert(s,3)
+    print(l)
