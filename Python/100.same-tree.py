c3cdbafac724eef72453b04f2c22a28a9560739d
郑康 <zhengkang@idss-cn.com>
2019-02-17 09:41:41 +0800

    100.same-tree.py

---
+++ Python/100.same-tree.py
@@ -0,0 +1,82 @@
+#
+# @lc app=leetcode id=100 lang=python3
+#
+# [100] Same Tree
+#
+# https://leetcode.com/problems/same-tree/description/
+#
+# algorithms
+# Easy (49.30%)
+# Total Accepted:    344.5K
+# Total Submissions: 698.8K
+# Testcase Example:  '[1,2,3]\n[1,2,3]'
+#
+# Given two binary trees, write a function to check if they are the same or
+# not.
+# 
+# Two binary trees are considered the same if they are structurally identical
+# and the nodes have the same value.
+# 
+# Example 1:
+# 
+# 
+# Input:     1         1
+# ⁠         / \       / \
+# ⁠        2   3     2   3
+# 
+# ⁠       [1,2,3],   [1,2,3]
+# 
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:     1         1
+# ⁠         /           \
+# ⁠        2             2
+# 
+# ⁠       [1,2],     [1,null,2]
+# 
+# Output: false
+# 
+# 
+# Example 3:
+# 
+# 
+# Input:     1         1
+# ⁠         / \       / \
+# ⁠        2   1     1   2
+# 
+# ⁠       [1,2,1],   [1,1,2]
+# 
+# Output: false
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p: 'TreeNode', q: 'TreeNode') -> 'bool':
+        if (p == q == None):
+            return True 
+        elif(p and not q):
+            return False
+        elif(q and not p):
+            return False
+        elif (p.val == q.val and p.left == q.left == None and p.right == q.right == None):
+            return True
+        elif(p.val == q.val):
+            left = self.isSameTree(p.left, q.left)
+            right = self.isSameTree(p.right, q.right)
+            if (left == False or right == False):
+                return False
+            return True
+        else:
+            return False
+        
