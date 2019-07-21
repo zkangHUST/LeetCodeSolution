77682067227dd14a0567244477939dd202de639c
zkangHUST <1473345713@qq.com>
2019-07-21 12:28:42 +0800

    240. Search a 2D Matrix II

---
+++ Python/240-1.py
@@ -0,0 +1,19 @@
+# 从右上角开始搜索
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+        row , col = 0, len(matrix[0]) - 1
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            if matrix[row][col] > target:
+                col -= 1
+            else :
+                row += 1
+        return False
