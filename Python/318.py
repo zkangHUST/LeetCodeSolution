1b4921682670cbdda2cff8806439d40a5f8d79e0
zkangHUST <1473345713@qq.com>
2019-08-17 23:44:29 +0800

    318. Maximum Product of Word Lengths

---
+++ Python/318.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        max = 0
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                tmp = self.check(words[i], words[j])
+                if tmp > max:
+                    max = tmp
+        return max
+    def check(self, a, b):
+        for c in a:
+            if c in b:
+                return 0
+        return len(a) * len(b)
+    
+        
