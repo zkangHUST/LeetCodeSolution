29a671fc3817af0f82863c5ced6c8439a6d4dad3
zkangHUST <1473345713@qq.com>
2019-08-17 20:17:40 +0800

    class Solution:

---
+++ Python/921.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minAddToMakeValid(self, S: str) -> int:
+        left = 0
+        right = 0
+        stack = []
+        for c in S:
+            if c == '(':
+                stack.append(c)
+                left += 1
+            else:
+                if len(stack) > 0 and stack[-1] == '(':
+                    left -= 1
+                    stack.pop()
+                else:
+                    right += 1
+                    stack.append(c)
+        return left + right
+        
