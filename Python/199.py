4b0e2f2a98fef18910d24d35e2e839e756d7b69c
vincent.zheng <vincent.zheng@zoom.us>
2019-08-20 19:50:59 +0800

    199. Binary Tree Right Side View

---
+++ Python/199.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        ans = []
+        if not root:
+            return 
+        stack = [root]
+        cnt = 1
+        nextcnt = 0
+        while stack:
+            if cnt > 0:
+                cnt -= 1
+                node = stack.pop(0)
+                if cnt == 0:
+                    ans.append(node.val)
+                if node.left:
+                    stack.append(node.left)
+                    nextcnt += 1
+                if node.right:
+                    stack.append(node.right)
+                    nextcnt += 1
+            else:
+                cnt = nextcnt
+                nextcnt = 0
+        return ans
+        
+        
+
+                
