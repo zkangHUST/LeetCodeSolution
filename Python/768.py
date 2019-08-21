8f28d5f08daaa46fc1b712c448c56c8304662bfd
zkangHUST <1473345713@qq.com>
2019-08-21 23:51:36 +0800

    768. Max Chunks To Make Sorted II

---
+++ Python/768.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        self.cnt = 1
+        self.split(arr)
+        return self.cnt
+    def split(self, arr):
+        flag = False
+        for i in range(1, len(arr)):
+            if max(arr[0:i]) <= min(arr[i:]):
+                self.cnt += 1
+                flag = True
+                break
+        if flag:
+            self.split(arr[i:])
+        return 
