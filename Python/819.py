1ffcb0967372d082b238abb64d1da46d8b00c5b3
zkangHUST <1473345713@qq.com>
2019-11-24 13:39:33 +0800

    819.Most Common Word

---
+++ Python/819.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        banset = set(banned)
+        for c in "!?',;.":
+            paragraph = paragraph.replace(c, " ")
+        count = collections.Counter(
+            word for word in paragraph.lower().split())
+
+        ans, best = '', 0
+        for word in count:
+            if count[word] > best and word not in banset:
+                ans, best = word, count[word]
+
+        return ans
