e146ec529897201622f3c0ac38f04c4d946bebf3
vincent.zheng <1473345713@qq.com>
2019-12-02 17:54:14 +0800

    update

--- Python/219.py
+++ Python/219.py
@@ -5,8 +5,6 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        num = [1, 2, 3, 1, 2, 3]
-        k = 2
         cnt = {}
         for i in range(len(nums)):
             if nums[i] in cnt and i - cnt[nums[i]] <= k:
