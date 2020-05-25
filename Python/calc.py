282d5187b09ca5fafb0fbbb720f7629d7a316c06
vincent.zheng <vincent.zheng@zoom.us>
2020-05-25 16:20:33 +0800

    del useless file

--- Python/calc.py
+++
@@ -1,6 +0,0 @@
-base = 143.616
-exp = 1.004
-sum = 0
-for i in range(22):
-    sum += base * pow(exp, i)
-print(sum)
--- Python/t.py
+++
@@ -1,8 +0,0 @@
-class Man:
-    def __init__(self):
-        self.name = None
-        self.age = -1
-
-class Chinese(Man):
-    def __init__(self):
-        self.id = None
