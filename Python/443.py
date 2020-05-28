e75d6a4ffd7a2aa1a32b0087dc8123084f078eb5
vincent <1473345713@qq.com>
2020-05-28 18:59:50 +0800

    443. 压缩字符串

--- Python/443.py
+++ Python/443.py
@@ -1,35 +1,17 @@
-class Solution(object):
-    def compress(self, chars):
-        """
-        :type chars: List[str]
-        :rtype: int
-        """
-        i = 0
-        cnt = 0
-        length = 0
-        tmp = []
-        while(i < len(chars)):
-            m = chars[i]
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        i, j = 0, 0
+        while j < len(chars):
+            ch = chars[j]
             cnt = 0
-            tmp.append(m)
-            while(i < len(chars) and chars[i] == m):
-                i += 1
+            while j < len(chars) and chars[j] == ch:
                 cnt += 1
-            length += len(m)
-            if (cnt > 1):
-                tmp +=  list(str(cnt))
-                length += len(str(cnt))
-        chars[:] = tmp[:]
-        return length
-
-
-res = Solution()
-# chars = ["a","a","b","b","c","c","c"]
-# chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
-chars =['a','a','a','a','a','a','a','a','a','a','a','a']
-# chars = ["a"]
-length = res.compress(chars)
-print(chars)
-print(length)
-            
-            
+                j += 1
+            chars[i] = ch
+            i += 1
+            if cnt > 1:
+                for c in str(cnt):
+                    chars[i] = c
+                    i += 1
+        # print(chars[0:i])
+        return i
