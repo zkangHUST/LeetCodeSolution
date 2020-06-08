03966960bfbe4ac70a49afd32bc2264cdc349bba
vincent <1473345713@qq.com>
2020-06-08 18:09:20 +0800

    1387. 将整数按权重排序

---
+++ Python/1387.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        self.weight = {}
+        res = [i for i in range(lo, hi + 1)]
+        res.sort(key = lambda x:self.getWeight(x))
+        return res[k - 1]
+    def getWeight(self, n):
+        if n in self.weight:
+            return self.weight[n]
+        if n == 1:
+            self.weight[n] = 0
+        elif n % 2 == 0:
+            self.weight[n] = 1 + self.getWeight(n // 2)
+        else:
+            self.weight[n] = 1 + self.getWeight(3 * n + 1)
+        return self.weight[n]
